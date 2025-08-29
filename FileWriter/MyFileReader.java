a1a8f8ceaadda36651e8fb7c082a3db492c44366
Andrew Theiss <andrew.theiss@gmail.com>
2025-08-29 12:01:59 -0400

    Maybe a bug

---
+++ FileWriter/MyFileReader.java
@@ -0,0 +1,9 @@
+public class MyFileReader {
+    public static void main(String[] args) {
+      try {
+        if (1 != 0) {
+            throw new Exception("1 is not equal to 0");
+        }
+      }
+    }
+}
--- FileWriter/MyFileWriter.java
+++ FileWriter/MyFileWriter.java
@@ -3,6 +3,7 @@ import java.io.*;
 import java.nio.file.*;
 import java.nio.charset.StandardCharsets;
 
+
 public class MyFileWriter {
     public static void main(String[] args) {
         String data = "Hello, World!";
@@ -20,7 +21,7 @@ public class MyFileWriter {
         }
 
         // 2. Using BufferedWriter
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName2))) {
+        try (BufferedWritsadfghjkgfdgsfaer bufferedWriter = new BufferedWriter(new FileWriter(fileName2))) {
             bufferedWriter.write(data);
         } catch (IOException e) {
             e.printStackTrace();
